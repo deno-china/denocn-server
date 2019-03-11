05139765691b07ce7b2df0e1991a8ed6563a0a7c
manyuanrong <416828041@qq.com>
2019-03-11 11:17:55 +0800

    update setup.ts

--- setup.ts
+++ setup.ts
@@ -1,28 +1,24 @@
-import { connect, sync } from "./models/main.ts";
+import { Client, dso } from "./deps.ts";
+import { sync } from "./models/main.ts";
 import { User } from "./models/user.ts";
 
-(async function () {
-    await connect();
-    await sync(true);
+(async function() {
+  const client = new Client();
+  await client.connect({
+    hostname: "127.0.0.1",
+    port: 3306,
+    debug: false,
+    username: "root",
+    password: "",
+    db: ""
+  });
+  await client.execute(`CREATE DATABASE IF NOT EXISTS denocn`);
+  await client.execute(`USE denocn`);
+  await dso.connect(client);
+  await sync(true);
 
-    await User.insert({
-        name: "admin",
-        password: "test",
-    });
-
-    await User.update(
-        { name: "admin", password: "test22" },
-        // { id: 1, },
-    );
-
-    await User.update(
-        { name: "admin", password: "test11" },
-        { id: 1, },
-    );
-
-    await User.update(
-        { name: "admin", password: "tes33", id: 1 },
-    );
-
-    // (await User.findById("")).
+  await User.insert({
+    name: "admin",
+    password: "test"
+  });
 })();
