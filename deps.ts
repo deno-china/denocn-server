a00c4695591c7d2a47f45970ded88b15d4d2b026
Enok <416828041@qq.com>
2019-06-19 11:21:30 +0800

    upgrade std deps

--- deps.ts
+++ deps.ts
@@ -1,7 +1,7 @@
 export * from "https://deno.land/x/dso@0.4.0/mod.ts";
 export { Application, Context, HttpError, Router, RouterContext, send, Status } from "https://deno.land/x/oak/mod.ts";
 export { runTests, test } from "https://deno.land/x/std/testing/mod.ts";
-export { assert, assertEquals } from "https://deno.land/x/testing@v0.9.0/asserts.ts";
+export { assert, assertEquals } from "https://deno.land/std/testing/asserts.ts";
 export { default as Marked } from "https://raw.githubusercontent.com/denolib/marked/master/main.ts";
 export { Template } from "https://raw.githubusercontent.com/zekth/deno_tiny_templates/master/mod.ts";
 
