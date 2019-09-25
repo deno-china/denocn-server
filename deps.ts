720f4d538f3c14a3f90954e07a624dc6e07ef4b8
Enok <416828041@qq.com>
2019-09-25 13:54:20 +0800

    upgrade deno version  & use aliyun docker registry

--- deps.ts
+++ deps.ts
@@ -1,10 +1,28 @@
-export { assert, assertEquals } from "https://deno.land/std/testing/asserts.ts";
-export { v4 as uuid } from "https://deno.land/std/uuid/mod.ts";
-export * from "https://deno.land/x/dso@0.4.5/mod.ts";
-export { Application, Context, HttpError, Router, RouterContext, send, Status } from "https://deno.land/x/oak@v2.2.0/mod.ts";
-export { runTests, test } from "https://deno.land/x/std/testing/mod.ts";
-export { connect as redisConnect, Redis } from "https://denopkg.com/keroxp/deno-redis/redis.ts";
-export { default as Marked } from "https://raw.githubusercontent.com/denolib/marked/master/main.ts";
-export { Template } from "https://raw.githubusercontent.com/zekth/deno_tiny_templates/master/mod.ts";
-import * as _colors from "https://deno.land/std/colors/mod.ts";
+export {
+  assert,
+  assertEquals
+} from "https://deno.land/std@v0.17.0/testing/asserts.ts";
+export { v4 as uuid } from "https://deno.land/std@v0.17.0/uuid/mod.ts";
+export * from "https://deno.land/x/dso@0.4.6/mod.ts";
+export {
+  Application,
+  Context,
+  HttpError,
+  Router,
+  RouterContext,
+  send,
+  Status
+} from "https://deno.land/x/oak@v2.5.0/mod.ts";
+export { runTests, test } from "https://deno.land/x/std@v0.17.0/testing/mod.ts";
+export {
+  connect as redisConnect,
+  Redis
+} from "https://denopkg.com/keroxp/deno-redis/redis.ts";
+export {
+  default as Marked
+} from "https://raw.githubusercontent.com/denolib/marked/master/main.ts";
+export {
+  Template
+} from "https://raw.githubusercontent.com/zekth/deno_tiny_templates/master/mod.ts";
+import * as _colors from "https://deno.land/std@v0.17.0/fmt/colors.ts";
 export const colors = _colors;
